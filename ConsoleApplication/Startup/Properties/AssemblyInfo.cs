cf6671f2317aef9d1e8d467d3591ea272f3ca170
Zlodey <a.n.owengm@gmail.com>
2015-02-20 12:18:38 +0600

    v.002

--- ConsoleApplication/Startup/Properties/AssemblyInfo.cs
+++ ConsoleApplication/Startup/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("task-1")]
+[assembly: AssemblyTitle("Startup")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("task-1")]
+[assembly: AssemblyProduct("Startup")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
