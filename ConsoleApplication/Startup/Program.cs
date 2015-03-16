9fa8a70aef6859a9b8d9609fd2648ac4d85d576f
Zlodey <a.n.owengm@gmail.com>
2015-03-16 16:33:09 +0600

    Task-3 v1.1

--- ConsoleApplication/Startup/Program.cs
+++ ConsoleApplication/Startup/Program.cs
@@ -6,46 +6,82 @@ using System.Threading.Tasks;
 
 namespace Startup  
 {
+    public class Program2
+    {
+        /*public void Conversion(string s)
+        {
+            int i1 = Convert.ToInt32(s);
+            PrintValue(i1); 
+        }*/
+
+        public void PrintValue(int i)
+        {
+           Console.WriteLine("Value = " + i);
+        }
+
+        public void PrintValue(bool b)
+        {
+            Console.WriteLine("Value = " + b);
+        }
+
+        public void PrintValue(char ch)
+        {
+            Console.WriteLine("Value = " + ch);
+        }
+
+        public void PrintValue(decimal d)
+        {
+            Console.WriteLine("Value = " + d);
+        }
+
+        public void PrintValue(double db)
+        {
+            Console.WriteLine("Value = " + db);
+        }
+
+        public void PrintValue(string s)
+        {
+            Console.WriteLine("Value = " + s);
+        }
+    }
+
     public class Program   
     {
         public static void Main()
         {
-			// TODO VS: Используй пустые строки, чтобы разбить  код на  логически связанные блоки. Ты сказал,  не любишь фарш из кода, и это хорошо.   
-			// TODO VS: Сплошная простыня из кода это другой пример плохого структурирования. 
-			// TODO VS: Как на какие куски разбить  код решай сам,  потом обсудим, если получится криво.  Один блок вполне может состоять и из одной строки,
-			// TODO VS: если она  логически отделена от  соседних. И кстати,  возможен другой порядок строк.
-            string s1 = "Bu-ga-ga"; // TODO VS: старый код можешь удалить. Если тебе потом он вдруг понадобится,  напишешь снова.
-            string s2 = "Metod 2:  ";// TODO VS: старый код можешь удалить
-            Method1(s1);// TODO VS: старый код можешь удалить
-            Method2(s2, 13, 13);// TODO VS: старый код можешь удалить
             int i = 10;
-            PrintValue(i);
             bool b = true;
-            PrintValue(b);
             char ch = 'C';
-            PrintValue(ch);
             decimal d = 123.45m;
-            PrintValue(d);
             double db = 99.44;
-            PrintValue(db);
             string s = "end";
+
+            PrintValue(i);                
+            PrintValue(b);            
+            PrintValue(ch);            
+            PrintValue(d);            
+            PrintValue(db);            
             PrintValue(s);
+            
+            Console.WriteLine("Blok 2:");
+            string s2 = "12g3";
+                        
+            Program2 arg = new Program2();
+            //arg.Conversion(s2); 
+            arg.PrintValue(i);
+            arg.PrintValue(b);
+            arg.PrintValue(ch);
+            arg.PrintValue(d);
+            arg.PrintValue(db);
+            arg.PrintValue(s);
+
+            /*Type t = ch.GetType();
+            Console.WriteLine(t.Name);
+            */
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
         
-        private static void Method1(string s)
-        {            
-            Console.WriteLine(s);            
-        } 
-        
-        private static void Method2(string q1, int a, int b)
-        {
-            string q = (a * b).ToString();
-            string q2 = q1 + q;
-            Method1(q2);            
-        }
-
         private static void PrintValue(int i)
         {
             Console.WriteLine("Value = " + i);   
@@ -56,7 +92,7 @@ namespace Startup
             Console.WriteLine("Value = " + b);
         }
 
-        private static void PrintValue(Char ch) // TODO VS [CG 1]
+        private static void PrintValue(char ch) 
         {
             Console.WriteLine("Value = " + ch);
         }
@@ -75,7 +111,6 @@ namespace Startup
         {
             Console.WriteLine("Value = " + s);
         }
-// TODO VS:  Ненужная пустая строка. Между фигурными скобками, закрывающими блоки разного уровня, строка не нужна [coding guidelines]
     }
 }
 
