9f66f875b3a399a82b804f98385919896af1bac4
Zlodey <a.n.owengm@gmail.com>
2015-03-24 14:04:10 +0600

    Task-3 v1.9 end.

--- ConsoleApplication/Startup/Program.cs
+++ ConsoleApplication/Startup/Program.cs
@@ -10,9 +10,6 @@ namespace Startup
     {
         public static void Main()
         {
-            Block1 arg1 = new Block1();
-            arg1.ExecuteBlock1();
-
             Block2 arg = new Block2(); 
             arg.ExecuteBlock2();
   
@@ -20,58 +17,6 @@ namespace Startup
             Console.ReadKey();
         }
     }
-
-    public class Block1
-    {
-        private const string constStringPrint = "Value = "; // TODO VS: [CG1]
-        
-        public void ExecuteBlock1()
-        {
-            int i = 10;
-            bool b = true;
-            char ch = 'C';
-            decimal d = 123.45m;
-            double db = 99.44;
-            string s = "end";
-
-            PrintValue(i);
-            PrintValue(b);
-            PrintValue(ch);
-            PrintValue(d);
-            PrintValue(db);
-            PrintValue(s);
-        }
-
-        private static void PrintValue(int i)
-        {
-            Console.WriteLine(constStringPrint + i); // TODO VS: [см. комментарий внизу]
-        }
-
-        private static void PrintValue(bool b)
-        {
-            Console.WriteLine(constStringPrint + b);
-        }
-
-        private static void PrintValue(char ch)
-        {
-            Console.WriteLine(constStringPrint + ch);
-        }
-
-        private static void PrintValue(decimal d)
-        {
-            Console.WriteLine(constStringPrint + d);
-        }
-
-        private static void PrintValue(double db)
-        {
-            Console.WriteLine(constStringPrint + db);
-        }
-
-        private static void PrintValue(string s)
-        {
-            Console.WriteLine(constStringPrint + s);
-        }
-    } 
     
     public class Block2
     {
@@ -150,14 +95,3 @@ namespace Startup
         }
     }
 }
-
-// TODO VS [CG1] Правила оформления кода - имена констант пишутся с большой буквы. переменные  с маленькой. то есть
-// TODO VS private string constStringPrint;
-// TODO VS private const ConstStringPrint;
-// TODO VS Если непонятно, в чем разница между константами и переменными, найди и выясни.  Разница достаточно существенная.
-
-// TODO VS [комментарий] Сделано все как надо, теперь у тебя есть два разных примера генрализации кода. Однако, генерализация в блоке 2
-// TODO VS значительно лучше. В блоке 1 все равно осталось много повторяющегося кода, и если  ты захочешь просто изменить строку,
-// TODO VS которую добавляешь  к значению, то в обоих блоках тебе надо сделать всего 1 изменение. Но представь, что ты хочешь изменить 
-// TODO VS саму функциональность вывода строки. Например, выводить две строки, или  делать какое-то более сложное форматирование текста,
-// TODO VS или вызывать еще какой-то метод перед выводом на экран... В этом случае недостаток повторяющегося кода в блоке 1 всплывет снова.
