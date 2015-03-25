c0bde5b4114d0bc84e9f9fae3b7569f319ff57f3
Zlodey <a.n.owengm@gmail.com>
2015-03-25 11:14:55 +0600

    Task-4 v1.2.1

--- ConsoleApplication/Startup/Program.cs
+++ ConsoleApplication/Startup/Program.cs
@@ -22,26 +22,91 @@ namespace Startup
     {
         public void ExecuteBlock2()
         {       
-            Console.WriteLine("Bloсk 2:");  
+            Console.WriteLine("Bloсk 2:");
 
-            var valueInt = 32;              
-            string stringValueInt = ParseToString(valueInt);
-            ParseToInt(stringValueInt);
+            var valueBool = true;
+            var valueChar = 'C';
+            var valueDecimal = 150.99m;
+            var valueDouble = 444.44;
+            var valueInt = 32;
+            var valueString = "end!!!";
+            
+            string[] stringValue = new string[6];
+            stringValue[0] = ParseToString(valueBool);
+            stringValue[1] = ParseToString(valueChar);
+            stringValue[2] = ParseToString(valueDecimal);
+            stringValue[3] = ParseToString(valueDouble);
+            stringValue[4] = ParseToString(valueInt);
+            stringValue[5] = ParseToString(valueString);
+
+            ParseToValue(stringValue);
         }
 
-        private static string ParseToString(int i) 
+        private static string ParseToString(bool b) 
+        {
+            return b.ToString();
+        }
+
+        private static string ParseToString(char ch)
+        {
+            return ch.ToString();
+        }
+
+        private static string ParseToString(decimal d)
+        {
+            return d.ToString();
+        }
+
+        private static string ParseToString(double db)
+        {
+            return db.ToString();
+        }
+
+        private static string ParseToString(int i)
         {
             return i.ToString();
         }
-
-        private static void ParseToInt(string inString) 
+        
+        private static string ParseToString(string s)
         {
-            PrintStringValue(int.Parse(inString), inString);        
+            return s;
         }
 
-        private static void PrintStringValue(int parseValueInt, string stringValue) 
+        private static void ParseToValue(string[] inString) 
         {
-            Console.WriteLine(string.Format("Значение определенного типа {0} строка {1}", parseValueInt, stringValue));
+            PrintStringValue(bool.Parse(inString[0]), inString[0]);
+            PrintStringValue(char.Parse(inString[1]), inString[1]);
+            PrintStringValue(decimal.Parse(inString[2]), inString[2]);
+            PrintStringValue(double.Parse(inString[3]), inString[3]);
+            PrintStringValue(int.Parse(inString[4]), inString[4]);
+            PrintStringValue(inString[5], inString[5]);
+        }
+        
+        private static void PrintStringValue(bool parseValue, string stringValue) 
+        {
+            Console.WriteLine(string.Format("Значение определенного типа {0} строка {1}", parseValue, stringValue));
+        }
+        
+        private static void PrintStringValue(char parseValue, string stringValue)
+        {
+            Console.WriteLine(string.Format("Значение определенного типа {0} строка {1}", parseValue, stringValue));
+        }
+
+        private static void PrintStringValue(decimal parseValue, string stringValue)
+        {
+            Console.WriteLine(string.Format("Значение определенного типа {0} строка {1}", parseValue, stringValue));
+        }
+        private static void PrintStringValue(double parseValue, string stringValue)
+        {
+            Console.WriteLine(string.Format("Значение определенного типа {0} строка {1}", parseValue, stringValue));
+        }
+        private static void PrintStringValue(int parseValue, string stringValue)
+        {
+            Console.WriteLine(string.Format("Значение определенного типа {0} строка {1}", parseValue, stringValue));
+        }
+        private static void PrintStringValue(string parseValue, string stringValue)
+        {
+            Console.WriteLine(string.Format("Значение определенного типа {0} строка {1}", parseValue, stringValue));
         }
     }
 }
