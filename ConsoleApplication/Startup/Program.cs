c20b99b3d61ae557ee6e196b42b5aaad15ec783b
Keen <keen.vs@gmail.com>
2015-03-24 18:54:22 -0700

    Code review

--- ConsoleApplication/Startup/Program.cs
+++ ConsoleApplication/Startup/Program.cs
@@ -22,25 +22,28 @@ namespace Startup
     {
         public void ExecuteBlock2()
         {       
-            Console.WriteLine("Bloсk 2:");
-            int valueInt = 32;            
-            string stringValueInt=PrintValue(valueInt);
+            Console.WriteLine("Bloсk 2:");  // TODO VS: после этой строки неплохо бы пустую строку поставить, т.к. дальше идет логически отделенный блок кода
+            int valueInt = 32;  // TODO VS: используй var            
+            string stringValueInt=PrintValue(valueInt); // TODO VS: форматирование - пробелы вокруг '='. посмотри как у тебя оно выше написано
             Convert(stringValueInt);
         }
 
-        private static string PrintValue(int i)
+        private static string PrintValue(int i) // TODO VS: метод сделан правильно, но название плохое - метод же ничего не печатает
         {
             return i.ToString();
         }
 
-        private static void PrintStringValue(int parseValueItn, string stringValue)
+        // TODO VS: этот мтод лучше перенести низ, после метода Convert, который его вызывает.  Код по возможности читается сверху вниз.
+		private static void PrintStringValue(int parseValueItn, string stringValue) // TODO VS: опечатка в названии параметра "parseValueItn", исправить
         {
             Console.WriteLine(string.Format("Значение определенного типа {0} строка {1}", parseValueItn, stringValue));
         }
 
-        private static void Convert(string inString) 
+        private static void Convert(string inString) // TODO VS: опять же неудачное название метода
         {
             PrintStringValue(int.Parse(inString), inString);        
         }
     }
 }
+
+ // TODO VS: Общие замечания.  Требовалось сделать перевод в строку и парсинг для всех  типов, использованных в  предыдущей задаче, не только для int.
