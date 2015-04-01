ad2eac1ac3c478952c8f9286804cb5f5d777d6c8
Zlodey <a.n.owengm@gmail.com>
2015-04-01 12:50:13 +0600

    Task-4 v1.8 +fix

--- ConsoleApplication/Startup/Program.cs
+++ ConsoleApplication/Startup/Program.cs
@@ -10,8 +10,8 @@ namespace Startup
     {
         public static void Main()
         {
-            Block2 arg = new Block2(); // TODO VS:  используй 'var', и дай переменной нормальное имя
-            arg.ExecuteBlock2();
+            var parseAnysengValue = new Block2(); 
+            parseAnysengValue.ExecuteBlock2();
   
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
@@ -34,25 +34,23 @@ namespace Startup
             var valueInt3 = 140;
             var testString = "4.12";
             
-            string[] stringValue = new string[5]; // TODO VS:  используй 'var' 
+            var stringValue = new string[5]; 
             stringValue[0] = ParseToString(valueBool);
             stringValue[1] = ParseToString(valueChar);
             stringValue[2] = ParseToString(valueDecimal);
             stringValue[3] = ParseToString(valueDouble);
             stringValue[4] = ParseToString(valueString);
 
-            string[] stringValueInt = new string[4]; // TODO VS: используй 'var'
+            var arbitraryString = "-3,5,abc,42,99.99,true,150"; 
+            var stringValueInt = new string[4]; 
             stringValueInt[0] = ParseToString(valueInt1);
             stringValueInt[1] = ParseToString(valueInt2);
             stringValueInt[2] = ParseToString(valueInt3);
             stringValueInt[3] = testString;
-            string arbitraryString = "-3,5,abc,42,99.99,true,150"; //TODO VS: use 'var'
+            string[] splitArguments = SplitArguments(arbitraryString);
 
             PrintStringValue(ParseStringToInt(stringValueInt), stringValueInt);
-
-			// TODO VS: большая степень вложенности  вызовов плохо читается, плюс у тебя повторяющийся вызов ParseStringToInt2(arbitraryString)
-			// TODO VS:  вынеси его в отдельную переменную и используй ее.
-            PrintStringValue(ParseStringToInt(ParseStringToInt2(arbitraryString)), ParseStringToInt2(arbitraryString));
+            PrintStringValue(ParseStringToInt(splitArguments), splitArguments);
 
             PrintStringValue(ParseStringToBool(stringValue[0]), stringValue[0]);
             PrintStringValue(ParseStringToChar(stringValue[1]), stringValue[1]);
@@ -111,38 +109,45 @@ namespace Startup
             return double.Parse(inString);
         }
 
-        private static string[] ParseStringToInt2(string inString) // TODO VS:  название Parse тут совсем не подходит, этот метод просто  разбивает строку
-																   // TODO VS:  на набор аргументов. назови SplitArguments
+        private static string[] SplitArguments(string inString) 
         {
-            char separator = ','; // TODO VS: идея вынести  разделитель в отдельную переменную весьма неплоха. но в таком случае стоит объявлять константу
-            string[] massString = inString.Split(separator); // TODO VS:  use 'var'
+            const char separator = ','; 
+            var massString = inString.Split(separator); 
             return massString;
         }
-		
+
+        private static int ParseOneValueInt(string inString)
+        {
+            int intParse;
+            bool result;
+            result = int.TryParse(inString, out intParse);
+            if (result)
+            {
+                if (intParse < 10)
+                {
+                    intParse = 10;
+                }
+
+                if (intParse > 100)
+                {
+                    intParse = 100;
+                }
+            }
+            else
+            {
+                intParse = -1;
+            }
+
+            return intParse;
+        }
+       
         private static int[] ParseStringToInt(string[] inString)
         {
-            int[] intParse = new int[inString.Length]; // TODO VS: use 'var'
-            bool result;
+            var intParse = new int[inString.Length];
+           
             for (int i = 0; i < inString.Length; i++)
             {
-                result = int.TryParse(inString[i], out intParse[i]);  // TODO VS: выдели все тело цикла в отдельный метод, парсящий  одно значение
-                if (result == false) // TODO VS: а не проще написать if (!result) ? переменная result у тебя и так имеет тип bool
-                {
-                    intParse[i] = -1;
-                }
-				//TODO VS: в блоке if ... else  пустая строка перед else не ставится, это логически один блок
-                else
-                {
-                    if (intParse[i] < 10)
-                    {
-                        intParse[i] = 10;
-                    }
-
-                    if (intParse[i] > 100)
-                    {
-                        intParse[i] = 100;
-                    }
-                }       
+                intParse[i] = ParseOneValueInt(inString[i]); 
             }
 
             return intParse;
@@ -175,10 +180,19 @@ namespace Startup
 
         private static void PrintStringValue(int[] parseValue, string[] stringValue)
         {
+            char separator = ',';
+            string stringWriteInt = "";
+            string stringWriteStr = "";
+
             for (int i = 0; i < parseValue.Length; i++)
             {
-                Console.WriteLine(string.Format("Значение типа int {0} строка {1}", parseValue[i], stringValue[i]));
+                stringWriteInt = string.Concat(stringWriteInt, separator, parseValue[i]);
+                stringWriteStr = string.Concat(stringWriteStr, separator, stringValue[i]);
             }
+
+            stringWriteInt = stringWriteInt.Remove(0, 1);
+            stringWriteStr = stringWriteStr.Remove(0, 1);
+            Console.WriteLine(string.Format("Значение типа int {0} строка {1}", stringWriteInt, stringWriteStr));
         }
 
         private static void PrintStringValue(string parseValue, string stringValue)
