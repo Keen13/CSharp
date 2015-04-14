143500732f5a0047a29550c3f4d30173a3b4fa1d
Zlodey <a.n.owengm@gmail.com>
2015-04-14 11:07:51 +0600

    task-5 v0.1

--- ConsoleApplication/Startup/Program.cs
+++ ConsoleApplication/Startup/Program.cs
@@ -24,39 +24,11 @@ namespace Startup
         {       
             Console.WriteLine("Bloсk 2:");
 
-            var valueBool = true;
-            var valueChar = 'C';
-            var valueDecimal = 150.99m;
-            var valueDouble = 444.44;
-            var valueInt1 = 32;
-            var valueString = "end!!!";
-            var valueInt2 = 6;
-            var valueInt3 = 140;
-            var testString = "4.12";
-            
-            var stringValue = new string[5]; 
-            stringValue[0] = ParseToString(valueBool);
-            stringValue[1] = ParseToString(valueChar);
-            stringValue[2] = ParseToString(valueDecimal);
-            stringValue[3] = ParseToString(valueDouble);
-            stringValue[4] = ParseToString(valueString);
-
             var arbitraryString = ReadString();
-            var stringValueInt = new string[4]; 
-            stringValueInt[0] = ParseToString(valueInt1);
-            stringValueInt[1] = ParseToString(valueInt2);
-            stringValueInt[2] = ParseToString(valueInt3);
-            stringValueInt[3] = testString;
             var splitArguments = SplitArguments(arbitraryString); 
 
-            PrintStringValue(ParseStringToInt(stringValueInt), stringValueInt);
             PrintStringValue(ParseStringToInt(splitArguments), splitArguments);
 
-            PrintStringValue(ParseStringToBool(stringValue[0]), stringValue[0]);
-            PrintStringValue(ParseStringToChar(stringValue[1]), stringValue[1]);
-            PrintStringValue(ParseStringToDecimal(stringValue[2]), stringValue[2]);
-            PrintStringValue(ParseStringToDouble(stringValue[3]), stringValue[3]);
-            PrintStringValue(ParseStringToString(stringValue[4]), stringValue[4]);
         }
 
         private static string ReadString()
@@ -65,59 +37,9 @@ namespace Startup
             return Console.ReadLine();
         }
 
-        private static string ParseToString(bool b) 
-        {
-            return b.ToString();
-        }
-
-        private static string ParseToString(char ch)
-        {
-            return ch.ToString();
-        }
-
-        private static string ParseToString(decimal d)
-        {
-            return d.ToString();
-        }
-
-        private static string ParseToString(double db)
-        {
-            return db.ToString();
-        }
-
-        private static string ParseToString(int i)
-        {
-            return string.Format("{0:000}", i);
-        }
-        
-        private static string ParseToString(string s)
-        {
-            return s;
-        }
-
-        private static bool ParseStringToBool(string inString)
-        {
-            return bool.Parse(inString);
-        }
-
-        private static char ParseStringToChar(string inString)
-        {
-            return char.Parse(inString);
-        }
-
-        private static decimal ParseStringToDecimal(string inString)
-        {
-            return decimal.Parse(inString);
-        }
-
-        private static double ParseStringToDouble(string inString)
-        {
-            return double.Parse(inString);
-        }
-
         private static string[] SplitArguments(string inString) 
         {
-            const char Separator = ','; 
+            char[] Separator = new char[] {'-', '+'}; 
             var massString = inString.Split(Separator); 
             return massString;
         }
@@ -133,94 +55,24 @@ namespace Startup
 
             return intParse;
         }
-
-        private static int ParseOneValueInt(string inString) 
+        
+        private static int ParseOneValueInt(string inString)
         {
             int intParse;
-            int findNumberSpase = FindNumberSpase(inString);
+            int BaseValue = 0;
 
-            if (findNumberSpase == 0)
+            if (int.TryParse(inString, out intParse)) 
             {
-                if (Utils.TryParseIntStrict(inString, out intParse)) // NB! Не изменять эту строку.
-                {
-                    return AdjustInt(intParse);
-                }
-            }
-            else
-            {
-                if (Utils.TryParseIntStrict(KillSpase(inString), out intParse)) // NB! Не изменять эту строку.
-                {
-                    return AdjustInt(intParse);
-                }
-            }
-                        
-            return -1;                      
-        }
-
-        private static int FindNumberSpase(string inString)
-        {
-            int i = 0;
-            int numberIndex = -1;
-            int amountSumbol = -1;
-            string findSymbol = " ";
-            
-            while (i != -1)
-            {
-                i = inString.IndexOf(findSymbol, numberIndex + 1);
-                numberIndex = i;
-                amountSumbol++; 
-            }
-            
-            return amountSumbol;             
-        }
-
-        private static string KillSpase(string inString)
-        {
-            const int stringConst = 1;
-            char findSymbol = ' ';
-            int i = inString.Length;
-            int endStringIndex = inString.Length - stringConst;
-            int startStringIndex = 0;
-            bool flag = true;
-
-            while (i != 0 )
-            {
-                i--;
-                if (inString[i] == findSymbol && flag)
-                {
-                    endStringIndex = i - 1;
-                }
-                else
-                {
-                    flag = false;
-                    i = 0;
-                }
-            }
-            
-            i=0;
-            flag = true;
-            
-            while (i != inString.Length)
-            {
-                if (inString[i] == findSymbol && flag)
-                {
-                    startStringIndex = i + 1;
-                    i++;
-                }
-                else
-                {
-                    flag = false;
-                    i++;
-                }                
+                return AdjustInt(intParse);
             }
 
-            return inString.Substring(startStringIndex, endStringIndex - startStringIndex + stringConst);
+            return BaseValue;
         }
 
         private static int AdjustInt(int intParse) 
         {
-            const int MaxValue = 100;
-            const int MinValue = 10;
+            const int MaxValue = 1000;
+            const int MinValue = -1000;
 
             if (intParse < MinValue)  
             {
@@ -235,31 +87,6 @@ namespace Startup
             return intParse;
         }
 
-        private static string ParseStringToString(string inString)
-        {
-            return inString;
-        }         
-        
-        private static void PrintStringValue(bool parseValue, string stringValue) 
-        {
-            Console.WriteLine(string.Format("Значение определенного типа {0} строка {1}", parseValue, stringValue));
-        }
-        
-        private static void PrintStringValue(char parseValue, string stringValue)
-        {
-            Console.WriteLine(string.Format("Значение определенного типа {0} строка {1}", parseValue, stringValue));
-        }
-
-        private static void PrintStringValue(decimal parseValue, string stringValue)
-        {
-            Console.WriteLine(string.Format("Значение определенного типа {0} строка {1}", parseValue, stringValue));
-        }
-
-        private static void PrintStringValue(double parseValue, string stringValue)
-        {
-            Console.WriteLine(string.Format("Значение определенного типа {0} строка {1}", parseValue, stringValue));
-        }
-
         private static void PrintStringValue(int[] parseValue, string[] stringValue) 
         {
             const string Separator = ","; 
@@ -269,9 +96,5 @@ namespace Startup
             Console.WriteLine(string.Format("Значение типа int {0} строка {1}", stringWriteInt, stringWriteStr));
         }
 
-        private static void PrintStringValue(string parseValue, string stringValue)
-        {
-            Console.WriteLine(string.Format("Значение определенного типа {0} строка {1}", parseValue, stringValue));
-        }
     }
 }
