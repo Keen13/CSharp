6af4c4e1a6830d89bf9319d3f422af04a199dac7
unknown <a.n.owengm@gmsil.com>
2015-06-20 16:56:54 +0600

    Task-6 v0.96

---
+++ ConsoleApplication/Startup/Interface.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Startup
+{
+    public class Interface
+    {
+        private const string EscapeString = "end";
+        private static readonly char[] FloodSymbols = { ' ', '.', '!' };
+
+        public void ExecuteCalculator()
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+            while (true)
+            {
+                var arbitraryString = ReadString();
+                if (arbitraryString.Trim(FloodSymbols) == EscapeString)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Ответ : {0}", Calculator.Sum(arbitraryString));
+            }
+
+            Console.WriteLine("Спасибо что воспользовались этой программой.");
+        }
+
+        private static string ReadString()
+        {
+            Console.WriteLine(
+                "Введите арифметический пример произвольной длины на сложение и вычитание \nцелых чисел или end для выхода :");
+            return Console.ReadLine();
+        }
+    }
+}
