734221c86475d998c6727daeca2c5d63321408b3
Zlodey <a.n.owengm@gmail.com>
2015-08-07 16:25:07 +0600

    Task-7 v 0.0.0.2.2

--- ConsoleApplication/PaymentSystemApi/PaymentInterface.cs
+++ ConsoleApplication/PaymentSystemApi/PaymentInterface.cs
@@ -8,22 +8,46 @@ namespace PaymentSystemApi
 {
     public class PaymentInterface
     {
+        private const string BankID1 = "VMPX";
+
+        private const string BankID2 = "OBRX";
+
+        private const string BankID3 = "SBRX";
+        private const ConsoleKey KeyExit = ConsoleKey.Escape;
+        
         public void Run()
         {
-            const ConsoleKey KeyExit = ConsoleKey.Escape;
-            string arbitraryString;
+            Console.WriteLine("Выбирите ваш бак: \n1)Банк Вампириал \n2)Банк Обирон \n3)Сбербанк России");
             
             while (true)
             {
                 var keyInfo = Console.ReadKey(true);
-                arbitraryString = ReadString();
-                if (KeyExit == keyInfo.Key)
+                if (keyInfo.Key == KeyExit)
                 {
                     break;
                 }
-            }
 
-            Console.WriteLine("Спасибо что воспользовались этой программой {0}.", arbitraryString);
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.D1:
+                        {
+                            Console.WriteLine("выбран пункт 1");
+                            break;
+                        }
+                    
+                    case ConsoleKey.D2:
+                        {
+                            Console.WriteLine("выбран пункт 2");
+                            break;
+                        }
+                    
+                    case ConsoleKey.D3:
+                        {
+                            Console.WriteLine("выбран пункт 3");
+                            break;
+                        }
+                }
+            }
         }
 
         private static string ReadString()
@@ -31,6 +55,26 @@ namespace PaymentSystemApi
             Console.WriteLine(
                 "Введите арифметический пример произвольной длины на сложение и вычитание \nцелых чисел или end для выхода :");
             return Console.ReadLine();
+        }
+
+        private static string ChooseBank(int number)
+        {
+            if (number == 1)
+            {
+                return BankID1;
+            }
+
+            if (number == 2)
+            {
+                return BankID2;
+            }
+
+            if (number == 3)
+            {
+                return BankID3;
+            }
+
+            return "0";
         }
     }
 }
